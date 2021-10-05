83d400f5c59fc80e0dfb59e2d9a17fd11038900c
frankstark <franklinluiz2000@gmail.com>
2021-10-04 21:59:59 -0300

    adicionado apps usuarios e porteiros

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
-    'usuarios'
+    'usuarios',
+    'porteiros'
 ]
 
 MIDDLEWARE = [
@@ -84,6 +85,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "usuarios.Usuario"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
